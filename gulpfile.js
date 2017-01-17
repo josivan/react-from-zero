7aabfc7e7543c44dcf1d122dbbc26ff8b1ac4c85
josivan <josivanps@gmail.com>
2017-01-17 01:10:12 -0200

    Tryn to generate files from jsx.

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,17 @@
-const gulp = require('gulp');
+const gulp    = require('gulp'),
+      rollup  = require('gulp-rollup');
+
+
+gulp.task('bundle', () => {
+  gulp.src('./src/**/*.js')
+    .pipe(rollup({
+      entry: './src/index3.js',
+      format: "iife",
+      plugins: [
+        require("rollup-plugin-babel")({
+          "plugins": ["external-helpers"]
+        })
+      ]
+    }))
+    .pipe(gulp.dest('./dest'));
+});
