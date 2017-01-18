9c85c29323750d421460af25f917b79bbb8e0d03
Josivan Pereira de Souza <jpsouza@scopus.com.br>
2017-01-18 17:05:06 -0200

    Using browserify.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,27 @@
-const gulp      = require('gulp'),
-      babel     = require('gulp-babel'),
-      polyfill  = require('babel-polyfill');
+const babelify    = require('babelify'),
+      browserify  = require('browserify'),
+      gulp        = require('gulp'),
+      babel       = require('gulp-babel');
 
 
 gulp.task('bundle', () => {
   gulp.src('./src/**/*.js')
     .pipe(babel())
     .pipe(gulp.dest('./dest'));
+});
+
+gulp.task('jsx', () => {
+  browserify({
+    entries: 'src/index3.jsx',
+    extensions: ['.jsx'],
+    debug: true
+  })
+  .transform(babelify)
+  .bundle()
+  .on('error', err => {
+    util.log("Browserify Error", util.colors.red(err.message))
+  })
+  .pipe(source('index.js'))
+  .pipe(buffer())
+  .pipe(gulp.dest('./dest'));
 });
