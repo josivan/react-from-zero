d1dbedb677fcd59f19b7aa1901ba5f0a12b4d362
Josivan Pereira de Souza <jpsouza@scopus.com.br>
2017-01-19 09:18:58 -0200

    Browserifying.

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,42 @@
+'use strict';
+
 const babelify    = require('babelify'),
       browserify  = require('browserify'),
       gulp        = require('gulp'),
+      reactify    = require('reactify'),
+      source      = require('vinyl-source-stream'),
       babel       = require('gulp-babel');
 
 
 gulp.task('bundle', () => {
-  gulp.src('./src/**/*.js')
+  gulp.src('./src/**/*.js*')
     .pipe(babel())
     .pipe(gulp.dest('./dest'));
 });
 
-gulp.task('jsx', () => {
-  browserify({
+gulp.task('_jsx', () => {
+  var b = browserify({
     entries: 'src/index3.jsx',
     extensions: ['.jsx'],
-    debug: true
-  })
-  .transform(babelify)
-  .bundle()
-  .on('error', err => {
-    util.log("Browserify Error", util.colors.red(err.message))
-  })
-  .pipe(source('index.js'))
-  .pipe(buffer())
-  .pipe(gulp.dest('./dest'));
+    debug: true,
+    transform: [reactify]
+  });
+
+  return b.bundle()
+    .pipe(source('./src'))
+    .pipe(bundle())
+    .pipe(gulp.dest('./dest'));
+});
+
+gulp.task('jsx', () => {
+  var b = browserify({
+    entries: 'dest/index3.js',
+    extensions: ['.js'],
+    debug: true,
+    transform: [reactify]
+  });
+
+  return b.bundle()
+    .pipe(source('./dest/index3.js'))
+    .pipe(gulp.dest('./bundle'));
 });
